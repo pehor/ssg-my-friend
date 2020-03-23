91966ef80c91e2add23066bde22d762561d374ca
Peter Horvath <>
2020-03-23 22:46:28 +0000

    Added json api endpoint Added static page export option

---
+++ pages/api/users.js
@@ -0,0 +1,6 @@
+// JSON API serving up users
+export default (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ name: 'John Doe' }))
+}
