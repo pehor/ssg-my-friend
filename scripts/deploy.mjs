1c39f8a2f2989341da43ee5920c3515a6003d275
Peter Horvath <>
2020-03-27 20:23:28 +0000

    Added example s3 file upload

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,12 +1,39 @@
 import AWS from 'aws-sdk'
+import util from 'util'
 
-AWS.config.getCredentials((err) => {
-  if (err) console.log(err.stack)
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId)
-    console.log("Secret access key:", AWS.config.credentials.secretAccessKey)
+const S3BucketName = 'psys-serve-test'
+const S3ApiVersion = '2006-03-01'
+
+const _setup = async () => {
+  try {
+    await util.promisify(AWS.config.getCredentials)
+  } catch (err) {
+    console.log(err.stack)
   }
-})
+  // we have now:
+  // AWS.config.credentials.accessKeyId
+  // AWS.config.credentials.secretAccessKey
+}
 
-export default {}
+const main = async () => {
+  await _setup()
+
+  const S3 = new AWS.S3({ apiVersion: S3ApiVersion })
+
+  const objectParams = {
+    Bucket: S3BucketName,
+    Key: 'testFile2',
+    Body: 'Hello World!'
+  }
+
+  console.log('Uploading file to S3...')
+  try {
+    await S3.putObject(objectParams).promise()
+  } catch (err) {
+    console.log(err, 'Error uploading file to S3')
+    return
+  }
+  console.log('Successfully uploaded file to S3')
+}
+
+main()
