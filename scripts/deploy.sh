da6372de0d0ab5b6c0dbba40093e65a755244163
Peter Horvath <>
2020-04-08 19:07:10 +0100

    Improved deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # You need to have a file in your home directory called
 # .ssg-config.sh
-# and you need to export the following AWS confgi envs
+# and in it, you need to export the following AWS config envs
 # export S3_BUCKET=[your bucket]
 # export AWS_ACCESS_KEY_ID=[your aws key id]
 # export AWS_SECRET_ACCESS_KEY=[your aws secret]
 # export AWS_DEFAULT_REGION=[your aws region]
-source ~/.ssg-config.sh
+#
+# Alternatively you can specify a file path as the first param to point to a different file.
+
+CONFIG=${1:-~/.ssg-config.sh}
+echo "using config at $CONFIG"
+source $CONFIG
 node ./node_modules/s3-redeploy/bin/s3-redeploy.js --bucket $S3_BUCKET --cwd ./out
